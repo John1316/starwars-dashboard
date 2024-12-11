11971c8c84bed19408ad72cc48f9461b0d3d9d13
John1316 <johnmaher179@gmail.com>
2024-12-11 15:46:15 +0200

    add fix film context

--- src/types/film.d.ts
+++ src/types/film.d.ts
@@ -32,6 +32,16 @@ type Film = {
     error: string | null;
     isModalOpen: boolean;
   };
+    // Action types
+    type ActionTypeFilms =
+    | { type: 'SET_FILMS'; payload: Film[] }
+    | { type: 'SET_SELECTED_FILMS'; payload: Film | null }
+    | { type: 'SET_CURRENT_PAGE'; payload: number }
+    | { type: 'SET_TOTAL_PAGES'; payload: number }
+    | { type: 'SET_SEARCH_QUERY'; payload: string }
+    | { type: 'SET_LOADING'; payload: boolean }
+    | { type: 'SET_ERROR'; payload: string | null }
+    | { type: 'TOGGLE_MODAL'; payload: boolean };
   interface FilmContextValue extends FilmContextState {
     // Actions
     fetchFilms: (page?: number, search?: string) => Promise<void>;
@@ -40,7 +50,7 @@ type Film = {
     setSelectedFilm: (Film: Film | null) => void;
     toggleModal: (isOpen: boolean) => void;
     clearError: () => void;
-  }
+}
   type FilmModalProps ={
     film: Film | null
   }
